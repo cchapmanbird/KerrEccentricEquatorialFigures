15ff63f5798a610a52d4153c7858fa6c43b01e90
Philip-Lynch <phil.lynch25@gmail.com>
2025-04-08 17:34:13 +0200

    Updated scripts to include directional downsampling too

--- scripts/Flux_Interpolation_Resolution/DownsampledFluxesDataGeneration.py
+++ scripts/Flux_Interpolation_Resolution/DownsampledFluxesDataGeneration.py
@@ -31,6 +31,14 @@ downsampled_4 = EMRIInspiral(func=KerrEccEqFlux,downsample=[(skip_points, skip_p
 skip_points = 8
 downsampled_8 = EMRIInspiral(func=KerrEccEqFlux,downsample=[(skip_points, skip_points, skip_points), (skip_points, skip_points, skip_points)])
 
+kip_points = 2
+downsampled_u = EMRIInspiral(func=KerrEccEqFlux, downsample=[(skip_points, 1, 1), (skip_points, 1, 1)])
+
+skip_points = 2
+downsampled_w = EMRIInspiral(func=KerrEccEqFlux,downsample=[(1, skip_points, 1), (1, skip_points, 1)])
+
+skip_points = 2
+downsampled_z = EMRIInspiral(func=KerrEccEqFlux,downsample=[(1, 1, skip_points), (1, 1, skip_points)])
 
 # Jonathan's randomised parameters code
 
@@ -85,7 +93,7 @@ def gen_parameters(NEVAL, duration, seed_in=314159):
             updated_params[4] = e
             updated_params[3] = get_p_at_t(
                 full_flux,
-                duration * 0.99,
+                duration * 1.01,
                 [updated_params[0], updated_params[1], updated_params[2], updated_params[4], 1.0],
                 index_of_p=3,
                 index_of_a=2,
@@ -108,7 +116,7 @@ def gen_parameters(NEVAL, duration, seed_in=314159):
 traj_pars = gen_parameters(N, T, seed_in=314159)[0]
 
 # Calculate the dephasings
-dephasings = np.zeros([len(traj_pars),3])
+dephasings = np.zeros([len(traj_pars),6])
 
 for i in range(len(traj_pars)):
 
@@ -117,10 +125,17 @@ for i in range(len(traj_pars)):
     t2, p2, e2, xI2, Phi_phi2, Phi_theta2, Phi_r2  = downsampled_2(*traj_pars[i], T=T,err=1e-12)
     t4, p4, e4, xI4, Phi_phi4, Phi_theta4, Phi_r4  = downsampled_4(*traj_pars[i], T=T,err=1e-12)
     t8, p8, e8, xI8, Phi_phi8, Phi_theta8, Phi_r8  = downsampled_8(*traj_pars[i], T=T,err=1e-12)
+    tu, pu, eu, xIu, Phi_phiu, Phi_thetau, Phi_ru  = downsampled_u(*traj_pars[i], T=T,err=1e-12)
+    tw, pw, ew, xIw, Phi_phiw, Phi_thetaw, Phi_rw  = downsampled_w(*traj_pars[i], T=T,err=1e-12)
+    tz, pz, ez, xIz, Phi_phiz, Phi_thetaz, Phi_rz  = downsampled_z(*traj_pars[i], T=T,err=1e-12)
+
 
     dephasings[i,0] = np.log10(np.abs(Phi_phi[-1] - Phi_phi2[-1]))
     dephasings[i,1] = np.log10(np.abs(Phi_phi[-1] - Phi_phi4[-1]))
     dephasings[i,2] = np.log10(np.abs(Phi_phi[-1] - Phi_phi8[-1]))
+    dephasings[i,3] = np.log10(np.abs(Phi_phi[-1] - Phi_phiu[-1]))
+    dephasings[i,4] = np.log10(np.abs(Phi_phi[-1] - Phi_phiw[-1]))
+    dephasings[i,5] = np.log10(np.abs(Phi_phi[-1] - Phi_phiz[-1]))
 
 np.savetxt('DownsampledFluxesData.txt', dephasings)
 
